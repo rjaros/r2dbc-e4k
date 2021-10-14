ee5af09f85b8572a4c9b77e7cdc55236cb79c380
Robert Jaros <rjaros@finn.pl>
2021-10-14 09:12:16 +0200

    Upgrade dependencies

--- buildSrc/src/main/kotlin/Shared.kt
+++ buildSrc/src/main/kotlin/Shared.kt
@@ -1,4 +1,4 @@
-import de.marcphilipp.gradle.nexus.NexusPublishExtension
+import io.github.gradlenexus.publishplugin.NexusPublishExtension
 import org.gradle.api.Project
 import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.publish.maven.MavenPom
